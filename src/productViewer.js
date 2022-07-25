ba90ee3c2fd41ac0335fe2f0a8146008f0305997
Silviu Marinescu <f.silviumarinescu@gmail.com>
2022-07-25 03:15:51 +0300

    wip

--- src/productViewer.js
+++ src/productViewer.js
@@ -3,11 +3,6 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { RoomEnvironment } from "three/examples/jsm/environments/RoomEnvironment.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-var camera;
-var scene;
-
-const loader = new GLTFLoader();
-
 const productViewer = {
   container: null,
   image: null,
@@ -15,8 +10,11 @@ const productViewer = {
   model: null,
   orbitControls: null,
   renderer: null,
+  camera: null,
+  scene: null,
   loadObject: async function (path) {
     return new Promise((a) => {
+      const loader = new GLTFLoader();
       loader.load(path, (gltf) => {
         a(gltf);
       });
@@ -26,25 +24,25 @@ const productViewer = {
     this.container = container;
     this.image = image;
     this.product = product;
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(
       50,
       this.container.clientWidth / this.container.clientHeight,
       1,
       5000
     );
-    camera.position.z = 2000;
+    this.camera.position.z = 2000;
 
-    scene.add(camera);
+    this.scene.add(this.camera);
 
     const light1 = new THREE.AmbientLight(0xffffff, 0.3);
     light1.name = "ambient_light";
-    scene.add(light1);
+    this.scene.add(light1);
 
     const light2 = new THREE.DirectionalLight(0xffffff, 0.8 * Math.PI);
     light2.position.set(0.5, 0, 0.866);
     light2.name = "main_light";
-    scene.add(light2);
+    this.scene.add(light2);
 
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
@@ -76,19 +74,22 @@ const productViewer = {
     ];
     const reflectionCube = new THREE.CubeTextureLoader().load(urls);
     reflectionCube.mapping = THREE.CubeRefractionMapping;
-    scene.background = reflectionCube;
+    this.scene.background = reflectionCube;
 
-    scene.environment = pmremGenerator.fromScene(
+    this.scene.environment = pmremGenerator.fromScene(
       new RoomEnvironment(),
       0.04
     ).texture;
 
     this.container.appendChild(this.renderer.domElement);
 
-    this.orbitControls = new OrbitControls(camera, this.renderer.domElement);
+    this.orbitControls = new OrbitControls(
+      this.camera,
+      this.renderer.domElement
+    );
 
     this.orbitControls.addEventListener("change", () => {
-      this.renderer.render(scene, camera);
+      this.renderer.render(this.scene, this.camera);
     });
 
     this.model = (await this.loadObject(this.product)).scene;
@@ -105,22 +106,23 @@ const productViewer = {
     this.orbitControls.enablePan = false;
     this.orbitControls.minPolarAngle = Math.PI / 4;
     this.orbitControls.maxPolarAngle = Math.PI / 2;
-    camera.position.copy(center);
-    camera.position.y += size / 2 + camera.position.y;
+    this.camera.position.copy(center);
+    this.camera.position.y += size / 2 + this.camera.position.y;
     this.orbitControls.target = new THREE.Vector3(center.x, sz.y / 2, center.z);
-    camera.updateProjectionMatrix();
+    this.camera.updateProjectionMatrix();
     await this.setImage(this.image);
-    scene.add(this.model);
+    this.scene.add(this.model);
     this.onWindowResize();
     this.orbitControls.update();
-    this.renderer.render(scene, camera);
+    this.renderer.render(this.scene, this.camera);
     window.addEventListener("resize", () => {
       this.onWindowResize();
     });
   },
   onWindowResize: function () {
-    camera.aspect = this.container.clientWidth / this.container.clientHeight;
-    camera.updateProjectionMatrix();
+    this.camera.aspect =
+      this.container.clientWidth / this.container.clientHeight;
+    this.camera.updateProjectionMatrix();
 
     this.renderer.setSize(
       this.container.clientWidth,
@@ -142,12 +144,11 @@ const productViewer = {
   setImage: async function (image) {
     this.image = image;
     this.imageTexture = await this.convertImageToTexture(this.image);
-
     this.model.traverse((obj) => {
       if (obj instanceof THREE.Mesh && obj.name === "Mug_Porcelain_PBR001_0")
         obj.material.map = this.imageTexture;
     });
-    this.renderer.render(scene, camera);
+    this.renderer.render(this.scene, this.camera);
   },
 };
 
