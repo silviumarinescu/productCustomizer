98c151bec0b7aa5ca9bb45dbd2b2f9900ebdc93e
Silviu Marinescu <f.silviumarinescu@gmail.com>
2023-04-11 06:24:20 -0400

    reset for demo

--- src/productViewer.js
+++ src/productViewer.js
@@ -21,17 +21,34 @@ const productViewer = {
       });
     });
   },
-
-  init: async function ({ container, image, product, cameraUpdate }) {
+  setBackground: function () {
+    const path =
+      "https://threejs.org/examples/textures/cube/SwedishRoyalCastle/";
+    const format = ".jpg";
+    const urls = [
+      path + "px" + format,
+      path + "nx" + format,
+      path + "py" + format,
+      path + "ny" + format,
+      path + "pz" + format,
+      path + "nz" + format,
+    ];
+    const reflectionCube = new THREE.CubeTextureLoader().load(urls);
+    reflectionCube.mapping = THREE.CubeRefractionMapping;
+    this.scene.background = reflectionCube;
+  },
+  init: async function ({ container, image, product }) {
     this.container = container;
     this.image = image;
+    this.product = product;
     this.scene = new THREE.Scene();
     this.camera = new THREE.PerspectiveCamera();
 
     this.scene.add(this.camera);
 
+
+
     this.renderer = new THREE.WebGLRenderer({
-      alpha: true,
       antialias: true,
       logarithmicDepthBuffer: true,
     });
@@ -45,28 +62,54 @@ const productViewer = {
     this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
     this.renderer.toneMappingExposure = 0.7;
 
+    this.setBackground();
+
     this.container.appendChild(this.renderer.domElement);
 
     this.orbitControls = new OrbitControls(
       this.camera,
       this.renderer.domElement
     );
-    this.orbitControls.addEventListener("change", async () => {
-      cameraUpdate(
-        this.camera.position.x,
-        this.camera.position.y,
-        this.camera.position.z,
-      );
+    this.orbitControls.addEventListener("change", () => {
       this.renderer.render(this.scene, this.camera);
     });
 
-    await this.setModel(product);
+    this.model = (await this.loadObject(this.product)).scene;
+    const box = new THREE.Box3().setFromObject(this.model);
+    const sz = box.getSize(new THREE.Vector3());
+    const size = sz.length();
+    const center = box.getCenter(new THREE.Vector3());
 
-    window.addEventListener("resize", async () => {
-      await this.onWindowResize();
+    const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+    this.scene.environment = pmremGenerator.fromScene(
+      new RoomEnvironment(),
+      0.04,
+      1
+    ).texture;
+
+    this.model.position.x = center.x * -1;
+    this.model.position.z = center.z * -1;
+    this.model.position.y = sz.y / 2 - center.y;
+    this.orbitControls.maxDistance = size * 5;
+    this.orbitControls.minDistance = size / 2;
+    this.orbitControls.enableZoom = false;
+    this.orbitControls.enablePan = false;
+    this.orbitControls.minPolarAngle = Math.PI / 4;
+    this.orbitControls.maxPolarAngle = Math.PI / 2;
+    this.camera.position.copy(center);
+    this.camera.position.y += size / 2 + this.camera.position.y;
+    this.orbitControls.target = new THREE.Vector3(center.x, sz.y / 2, center.z);
+    this.camera.updateProjectionMatrix();
+    await this.setImage(this.image);
+    this.scene.add(this.model);
+    this.onWindowResize();
+    this.orbitControls.update();
+    this.renderer.render(this.scene, this.camera);
+    window.addEventListener("resize", () => {
+      this.onWindowResize();
     });
   },
-  onWindowResize: async function () {
+  onWindowResize: function () {
     this.camera.aspect =
       this.container.clientWidth / this.container.clientHeight;
     this.camera.updateProjectionMatrix();
@@ -85,102 +128,17 @@ const productViewer = {
       const textureLoader = new THREE.TextureLoader(manager);
       texture = textureLoader.load(image);
       texture.encoding = THREE.sRGBEncoding;
-      texture.flipY = true;
+      texture.flipY = false;
     });
-  },
-  size: null,
-  sz: null,
-  center: null,
-  setCamera: async function (
-    positionX = 1,
-    positionY = 1,
-    positionZ = 1,
-    rotationX = 0,
-    rotationY = 0,
-    rotationZ = 0
-  ) {
-    this.model.position.x = this.center.x * -1;
-    this.model.position.z = this.center.z * -1;
-    this.model.position.y = this.sz.y / 2 - this.center.y;
-
-    this.camera.position.x = positionX;
-    this.camera.position.y = positionY;
-    this.camera.position.z = positionZ;
-
-    // this.camera.rotation.x = rotationX;
-    // this.camera.rotation.y = rotationY;
-    // this.camera.rotation.z = rotationZ;
-    // this.camera.lookAt(new THREE.Vector3(
-    //   rotationX,
-    //   rotationY,
-    //   rotationZ
-    // ));
-
-    // this.camera.updateProjectionMatrix();
-
-    // this.orbitControls.maxDistance = this.size * 2;
-    // this.orbitControls.minDistance = this.size;
-    // this.orbitControls.enableZoom = false;
-    // this.orbitControls.enablePan = false;
-    // this.orbitControls.minPolarAngle = Math.PI / 8;
-    // this.orbitControls.maxPolarAngle = Math.PI / 2;
-
-    // this.camera.lookAt(this.model);
-    // this.camera.position.copy(this.center);
-    // this.camera.position.y += this.size + this.camera.position.y;
-
-    // this.orbitControls.target = new THREE.Vector3(this.model.position.x, this.model.position.y, this.model.position.z);
-
-    this.orbitControls.target = new THREE.Vector3(
-      rotationX,
-      rotationY,
-      rotationZ
-    );
-
-    this.orbitControls.update();
-    this.renderer.render(this.scene, this.camera);
-  },
-  setModel: async function (model) {
-    if (this.model) this.scene.remove(this.model);
-    this.model = (await this.loadObject(model)).scene;
-    const box = new THREE.Box3().setFromObject(this.model);
-    this.sz = box.getSize(new THREE.Vector3());
-    this.size = this.sz.length();
-    this.center = box.getCenter(new THREE.Vector3());
-
-    const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
-    this.scene.environment = pmremGenerator.fromScene(
-      new RoomEnvironment(),
-      0.04,
-      1
-    ).texture;
-
-    await this.setCamera();
-    await this.setImage(this.image);
-    this.scene.add(this.model);
-    await this.onWindowResize();
-    this.orbitControls.update();
-    this.renderer.render(this.scene, this.camera);
   },
   setImage: async function (image) {
     this.image = image;
     this.imageTexture = await this.convertImageToTexture(this.image);
     this.model.traverse((obj) => {
-      if (obj instanceof THREE.Mesh && obj.name === "art_surface")
+      if (obj instanceof THREE.Mesh && obj.name === "Mug_Porcelain_PBR001_0")
         obj.material.map = this.imageTexture;
     });
     this.renderer.render(this.scene, this.camera);
-  },
-  async getImage(width = 300, height = 300) {
-    this.camera.aspect = width / height;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(width, height);
-    this.renderer.render(this.scene, this.camera, null, false);
-    const img = this.renderer.domElement.toDataURL("image/png");
-
-    await this.onWindowResize();
-
-    return img;
   },
 };
 
